b6c3f042f0986ce674bf7befd20c0434c244503a
fxpw <fxpw@outlook.com>
2025-10-29 13:14:33 +0300

    delete some code

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -4,7 +4,7 @@ const process = require('process');
 
 const config = require('config/config.js');
 
-const sequelize = new Sequelize(config.database, config.username, config.password);
+// const sequelize = new Sequelize(config.database, config.username, config.password);
 
 
 // module.exports = sequelize;
